f3912feaad6c6d07dc5efb130239a1905f0e749e
Daniel Heinrich <daniel.heinrich@connect.qut.edu.au>
2024-04-01 19:35:30 +1000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace MyApp
+{
+    internal class Program
+    {
+     
+
+        static void Main(string[] args)
+        {
+            //Ask the user if they want to send or recive 
+            Console.WriteLine("Are you Sending (0) or Reciving(1)");
+            int key = Convert.ToInt32(Console.ReadLine());
+            
+        }
+    }
+}
